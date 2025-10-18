ab54b163818d092c77d9d2e0f3484c7f948ea95f
Yan Bratushchik <whitebbit.entertainment@gmail.com>
2025-10-18 13:13:06 +0300

    lesson 6

--- Game DEMO/Assets/!Game/Scripts/Multiplayer/MultiplayerManager.cs
+++ Game DEMO/Assets/!Game/Scripts/Multiplayer/MultiplayerManager.cs
@@ -1,10 +1,15 @@
-﻿using Colyseus;
+﻿using _Game.Scripts.Player;
+using Colyseus;
+using UnityEngine;
 
 namespace _Game.Scripts.Multiplayer
 {
     public class MultiplayerManager : ColyseusManager<MultiplayerManager>
     {
         #region FIELDS SERIALIZED
+
+        [SerializeField] private PlayerUnit player;
+        [SerializeField] private Transform enemy;
 
         #endregion
 
@@ -26,7 +31,7 @@ namespace _Game.Scripts.Multiplayer
         protected override void OnDestroy()
         {
             base.OnDestroy();
-            
+
             _room.Leave();
         }
 
@@ -37,6 +42,27 @@ namespace _Game.Scripts.Multiplayer
         private async void Connect()
         {
             _room = await Instance.client.JoinOrCreate<State>("state_handler");
+            _room.OnStateChange += OnStateChange;
+        }
+
+        private void OnStateChange(State state, bool isFirstState)
+        {
+            if (!isFirstState) return;
+
+            var serverPlayer = state.players[_room.SessionId];
+            var position = new Vector3(serverPlayer.x - 200, 0, serverPlayer.y - 200) / 8;
+
+            var obj = Instantiate(player, position, Quaternion.identity);
+
+            state.players.ForEach(ForEachEnemy);
+        }
+
+        private void ForEachEnemy(string key, global::Player player)
+        {
+            if (key == _room.SessionId) return;
+            
+            var position = new Vector3(player.x - 200, 0, player.y - 200) / 8;
+            var obj = Instantiate(enemy, position, Quaternion.identity);
         }
 
         #endregion
